c178317828f4e87ccf4e208ef1d2de7c2808b30b
Joseph C Wang <joequant@gmail.com>
2023-09-24 16:59:12 +0800

    add balances

--- balances.py
+++ balances.py
@@ -1,5 +1,28 @@
 import requests
+from tenacity import retry, stop_after_attempt, wait_exponential
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential())
+def get_btc_balance(wallet_address: str):
+    try:
+        response = requests.get(
+            f'https://blockchain.info/rawaddr/{wallet_address}'
+        )
+        data = response.json()
+        balance = float(data['final_balance'])/100000000.0
+
+        price_url = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd'
+        price_response = requests.get(price_url)
+        price_data = price_response.json()
+        price_usd = price_data['bitcoin']['usd']
+        return {
+            'balance': balance,
+            'price_usd': price_usd
+        }
+    except requests.exceptions.HTTPError as e:
+        print(f"An error occurred: {e}")
+        return None
+
+@retry(stop=stop_after_attempt(3), wait=wait_exponential())
 def get_tron_balance(wallet_address):
     url = f'https://apilist.tronscanapi.com/api/account/wallet?address={wallet_address}'
     try:
@@ -7,44 +30,39 @@ def get_tron_balance(wallet_address):
         data = response.json()
         balance = float(data['data'][0]['balance'])
         price_usd = float(data['data'][0]['token_price_in_usd'])
-        balance_usd = balance * price_usd
-
-        result = {
+        return {
             'balance': balance,
-            'price_usd': price_usd,
-            'balance_usd': balance_usd
+            'price_usd': price_usd
         }
-        return result
-
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.HTTPError as e:
         print(f"An error occurred: {e}")
         return None
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential())
 def get_trc20_token_balance(wallet_address, token_contract_address):
-    url = f'https://apilist.tronscan.org/api/token_trc20/account?address={wallet_address}&contract={token_contract_address}'
+    url = f'https://apilist.tronscan.org/api/token_trc20/holders?holder_address={wallet_address}&contract_address={token_contract_address}'
 
     try:
         response = requests.get(url)
         data = response.json()
-        balance_token = int(data['balance']) / 10**data['decimals']
+        balance_token = int(data['trc20_tokens'][0]['balance']) / 10**6
 
         # Get the value in USD
         token_price_url = f'https://api.coingecko.com/api/v3/simple/token_price/tron?contract_addresses={token_contract_address}&vs_currencies=usd'
         token_price_response = requests.get(token_price_url)
         token_price_data = token_price_response.json()
-        token_usd_price = token_price_data[token_contract_address.lower()]['usd']
-        balance_usd = balance_token * token_usd_price
+        token_usd_price = token_price_data[token_contract_address]['usd']
 
-        result = {
+        return {
             'balance_token': balance_token,
-            'balance_usd': balance_usd
+            'price_usd': token_usd_price
         }
-        return result
 
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.HTTPError as e:
         print(f"An error occurred: {e}")
         return None
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential())
 def get_evm_balance(wallet_address, chain_url, coin, api_key):
     url = f'https://{chain_url}/api?module=account&action=balance&address={wallet_address}&tag=latest&apikey={api_key}'
     try:
@@ -58,22 +76,21 @@ def get_evm_balance(wallet_address, chain_url, coin, api_key):
         price_response = requests.get(price_url)
         price_data = price_response.json()
         price_usd = price_data[coin]['usd']
-        balance_usd = balance * price_usd
-
-        result = {
+        return {
             'balance': balance,
-            'price_usd': price_usd,
-            'balance_usd': balance_usd
+            'price_usd': price_usd
         }
-        return result
 
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.HTTPError as e:
         print(f"An error occurred: {e}")
         return None
 
-def get_evm_token_balance(wallet_address, token_contract_address):
-    api_key = 'YOUR_ETHERSCAN_API_KEY'  # Replace with your Etherscan API key
-    url = f'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress={token_contract_address}&address={wallet_address}&tag=latest&apikey={api_key}'
+@retry(stop=stop_after_attempt(3), wait=wait_exponential())
+def get_evm_token_balance(
+        wallet_address, chain_url, coin,
+        token_contract_address, api_key
+):
+    url = f'https://{chain_url}/api?module=account&action=tokenbalance&contractaddress={token_contract_address}&address={wallet_address}&tag=latest&apikey={api_key}'
 
     try:
         response = requests.get(url)
@@ -81,18 +98,14 @@ def get_evm_token_balance(wallet_address, token_contract_address):
         balance_token = int(data['result']) / 10**18
 
         # Get the value in USD
-        token_price_url = f'https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses={token_contract_address}&vs_currencies=usd'
+        token_price_url = f'https://api.coingecko.com/api/v3/simple/token_price/{coin}?contract_addresses={token_contract_address}&vs_currencies=usd'
         token_price_response = requests.get(token_price_url)
         token_price_data = token_price_response.json()
         token_usd_price = token_price_data[token_contract_address.lower()]['usd']
-        balance_usd = balance_token * token_usd_price
-
-        result = {
-            'balance_token': balance_token,
-            'balance_usd': balance_usd
+        return {
+            'balance': balance_token,
+            'price_usd': token_usd_price,
         }
-        return result
-
-    except requests.exceptions.RequestException as e:
+    except requests.exceptions.HTTPError as e:
         print(f"An error occurred: {e}")
         return None
--- generate_report.py
+++ generate_report.py
@@ -2,48 +2,72 @@
 import logging
 import os
 import pandas as pd
-import requests
 from dotenv import load_dotenv
 from truflation.data.pipeline import Pipeline
 from truflation.data.pipeline_details import PipeLineDetails
 from truflation.data.source_details import SourceDetails
 from truflation.data.export_details import ExportDetails
 from balances import get_evm_balance, get_tron_balance
+from balances import get_evm_token_balance, get_trc20_token_balance
+from balances import get_btc_balance
 
 load_dotenv()
 logger = logging.getLogger(__name__)
 PIPELINE_NAME = 'Virtual Asset report'
 ETHERSCAN_KEY = os.environ.get('ETHERSCAN_KEY')
 BSCSCAN_KEY = os.environ.get('BSCSCAN_KEY')
+CONTRACTS = {
+    'eth': {
+        'USDT': '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    },
+    'bnb': {
+        'USDT': '0x55d398326f99059fF775485246999027B3197955'
+    },
+    'trx': {
+        'USDT': 'TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t'
+    }
+}
 
 def get_value(row):
     wallet = row['wallet']
-    if row['blockchain'] == 'btc':
-        response = requests.get(
-            f'https://blockchain.info/rawaddr/{wallet}'
-        )
-        d = response.json()
-        print(d['final_balance'])
-    elif row['blockchain'] == 'eth':
+    chain = row['blockchain']
+    if chain == 'btc':
+        object = get_btc_balance(wallet)
+        print(object)
+    elif chain == 'eth':
         object = get_evm_balance(
             wallet, 'api.etherscan.io', 'ethereum', ETHERSCAN_KEY
         )
         print(object)
-        response = requests.get(
-            f'https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0x57d90b64a1a57749b0f932f1a3395792e12e7055&address={wallet}&tag=latest&apikey={ETHERSCAN_KEY}'
-        )
-        d = response.json()
-    elif row['blockchain'] == 'bnb':
+        for token, address in CONTRACTS[chain].items():
+            object = get_evm_token_balance(
+                wallet,
+                'api.etherscan.io',
+                'ethereum',
+                address,
+                ETHERSCAN_KEY
+            )
+            print(object)
+    elif chain == 'bnb':
         object = get_evm_balance(
             wallet, 'api.bscscan.com', 'binancecoin', BSCSCAN_KEY
         )
         print(object)
-        response = requests.get(
-            f'https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0x57d90b64a1a57749b0f932f1a3395792e12e7055&address={wallet}&tag=latest&apikey={BSCSCAN_KEY}'
-        )
-    elif row['blockchain'] == 'trx':
+        for token, address in CONTRACTS[chain].items():
+            object = get_evm_token_balance(
+                wallet,
+                'api.bscscan.com',
+                'binance-smart-chain',
+                address,
+                BSCSCAN_KEY
+            )
+            print(object)
+    elif chain == 'trx':
         object = get_tron_balance(wallet)
         print(object)
+        for token, address in CONTRACTS[chain].items():
+            object = get_trc20_token_balance(wallet, address)
+            print(object)
     return None
 
 def transformer(inputs: dict):
